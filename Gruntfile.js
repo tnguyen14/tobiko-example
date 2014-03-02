acba912f4531e693e6ec91d1f87fc4ca857fd6ef
Tri Nguyen <tri@tridnguyen.com>
2014-03-02 16:57:44 -0500

    add default Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = function(grunt) {
+	var path = require('path'),
+		config = grunt.file.readJSON('tobiko/config.json');
+	// load local tasks
+	grunt.loadTasks('tobiko/tasks');
+	// display execution time of grunt tasks
+	require('time-grunt')(grunt);
+	// load all grunt configs
+	require('load-grunt-config')(grunt, {
+		configPath: path.join(process.cwd(), 'tobiko/config'),
+		config: config
+	});
+	grunt.config.set('gh-pages', {
+		prod: {
+			options: {
+				base: '<%= buildPath %>',
+			},
+			src: ['**/*']
+		}
+	});
+}

8e49f1614bfb4d38fd0d916624aa549be703e78a
Tri Nguyen <tri@tridnguyen.com>
2014-03-02 19:14:29 -0500

    temporarily remove tobiko

--- tobiko/config/copy.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	build: {
-		files: [
-			{expand: true, src: ['CNAME'], dest: '<%= buildPath %>/'},
-			{expand: true, cwd: 'components', src: ['fancybox/source/**/*'], dest: '<%= buildPath %>/components'},
-			{expand: true, cwd: 'sass', src: 'assets/**/*', dest: '<%= buildPath %>/css/'}
-		]
-	}
-}

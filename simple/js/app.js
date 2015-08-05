0636fddc61fa42f9441df600f8c26c6f1c1bd797
Tri Nguyen <tri@tridnguyen.com>
2015-08-04 20:30:36 -0400

    update app.js to pass jshint

--- simple/js/app.js
+++ simple/js/app.js
@@ -1,8 +1,11 @@
+'use strict';
+
 var config = require('../config.json');
 var configDev = require('../config.dev.json');
 var $ = require('jquery');
 var _ = require('lodash');
 
+/* global page */
 if (page.env === 'dev') {
 	_.extend(config, configDev);
 }
